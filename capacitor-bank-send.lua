8d6450e633634ec6cfecb6a9db704da9d96c52fb
Sam Lee <me@shibuya-rin.moe>
2026-01-26 01:20:46 +0900

    use rednet

--- capacitor-bank-send.lua
+++ capacitor-bank-send.lua
@@ -1,18 +1,19 @@
 -- Config
 
-BANK_NAME_OR_LOCATION = "tile_blockcapacitorbank_name_1"
+BANK_NAME_OR_LOCATION = "tile_blockcapacitorbank_name_3"
 BANK_TOTAL_CAPACITY_RF = 1275000000
 
 MODEM_LOCATION = "right"
-CHAN = 100
+IDENTIFIER = "bank_primary"
 
 -- Init
 print("Starting... Hold Ctrl + T to terminate")
 
 cell = peripheral.wrap(BANK_NAME_OR_LOCATION)
-modem = peripheral.wrap(MODEM_LOCATION)
+modem = rednet.open(MODEM_LOCATION)
 
 while true do
+    print("Hold Ctrl + T to terminate.\n")
     local cell_max_energy = cell.getMaxEnergyStored()
     local cell_cur_energy = cell.getEnergyStored()
 
@@ -22,12 +23,11 @@ while true do
     print("Current Energy (RF): ", bank_cur_energy)
     print("Pct: ", energy_pct * 100)
 
-    modem.transmit(CHAN, CHAN + 1, string.format("max, %.2f", BANK_TOTAL_CAPACITY_RF))
-    modem.transmit(CHAN, CHAN + 1, string.format("cur, %.2f", bank_cur_energy))
-    modem.transmit(CHAN, CHAN + 1, string.format("pct, %.4f", energy_pct))
+    rednet.broadcast(string.format("%s,max,%.2f", IDENTIFIER, BANK_TOTAL_CAPACITY_RF))
+    rednet.broadcast(string.format("%s,cur,%.2f", IDENTIFIER, bank_cur_energy))
+    rednet.broadcast(string.format("%s,pct,%.4f", IDENTIFIER, energy_pct))
     os.sleep(3)
 
     term.clear()
-    print("Hold Ctrl + T to terminate.\n")
 end
 
